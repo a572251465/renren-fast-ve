34f67c307e96cd7ecd4859496163aba622f63828
lihaohao <2495041749@qq.com>
2022-11-14 14:05:03 +0800

    feat: 修改前端代码 可添加å前缀 识别微服务

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,11 +1,39 @@
-<script>
-export default {
-
-}
-</script>
-
 <template>
-  <div>111</div>
+  <div>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    };
+  },
+  methods: {
+    getCategory() {
+      this.$http({
+        url: this.$http.adornUrl('/pms/category/listWithTree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log("成功获取的类别数据：",data.data)
+        this.data = data.data
+      })
+    },
+    handleNodeClick(data) {
+      console.log(data);
+    }
+  },
+  created() {
+    this.getCategory();
+  }
+};
+</script>
 
 <style></style>
